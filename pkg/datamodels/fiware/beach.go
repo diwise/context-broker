5dbf47c63d7adea2f759c545b1fbed40f756da65
Isak Styf <isak.styf@masarinconsulting.se>
2022-06-13 20:14:26 +0200

    added beach type and a few more entity decorators

---
+++ pkg/datamodels/fiware/beach.go
@@ -0,0 +1,31 @@
+package fiware
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/diwise/context-broker/pkg/ngsild/types"
+	"github.com/diwise/context-broker/pkg/ngsild/types/entities"
+	dec "github.com/diwise/context-broker/pkg/ngsild/types/entities/decorators"
+)
+
+//NewBeach creates a new instance of Beach
+func NewBeach(entityID string, name string, decorators ...entities.EntityDecoratorFunc) (types.Entity, error) {
+
+	if len(decorators) == 0 {
+		return nil, fmt.Errorf("at least one property must be set in a beach entity")
+	}
+
+	if !strings.HasPrefix(entityID, BeachIDPrefix) {
+		entityID = BeachIDPrefix + entityID
+	}
+
+	decorators = append(decorators, dec.Name(name))
+
+	e, err := entities.New(
+		entityID, BeachTypeName,
+		decorators...,
+	)
+
+	return e, err
+}
--- pkg/ngsild/types/entities/decorators/decorators.go
+++ pkg/ngsild/types/entities/decorators/decorators.go
@@ -16,8 +16,26 @@ func Location(latitude, longitude float64) entities.EntityDecoratorFunc {
 	return entities.P("location", location)
 }
 
+func LocationLS(linestring [][]float64) entities.EntityDecoratorFunc {
+	location := geojson.CreateGeoJSONPropertyFromLineString(linestring)
+	return entities.P("location", location)
+}
+
+func LocationMP(multipolygon [][][][]float64) entities.EntityDecoratorFunc {
+	location := geojson.CreateGeoJSONPropertyFromMultiPolygon(multipolygon)
+	return entities.P("location", location)
+}
+
 func DateTime(name string, value string) entities.EntityDecoratorFunc {
 	return entities.P(name, properties.NewDateTimeProperty(value))
+}
+
+func Description(desc string) entities.EntityDecoratorFunc {
+	return Text("description", desc)
+}
+
+func Name(name string) entities.EntityDecoratorFunc {
+	return Text("name", name)
 }
 
 func Number(name string, value float64, decorators ...properties.NumberPropertyDecoratorFunc) entities.EntityDecoratorFunc {
@@ -28,12 +46,24 @@ func Number(name string, value float64, decorators ...properties.NumberPropertyD
 	return entities.P(name, np)
 }
 
+func Source(src string) entities.EntityDecoratorFunc {
+	return Text("source", src)
+}
+
 func Text(name string, value string) entities.EntityDecoratorFunc {
 	return entities.P(name, properties.NewTextProperty(value))
 }
 
+func DateCreated(timestamp string) entities.EntityDecoratorFunc {
+	return DateTime("dateCreated", timestamp)
+}
+
 func DateLastValueReported(timestamp string) entities.EntityDecoratorFunc {
 	return DateTime("dateLastValueReported", timestamp)
+}
+
+func DateModified(timestamp string) entities.EntityDecoratorFunc {
+	return DateTime("dateModified", timestamp)
 }
 
 func DateObserved(timestamp string) entities.EntityDecoratorFunc {
