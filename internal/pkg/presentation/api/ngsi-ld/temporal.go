1d74024f983f996a6b65daa10a3b91fa2a42e9ba
Isak Styf <isak.styf@masarinconsulting.se>
2022-12-02 15:03:40 +0100

    add an entitytemporal type with marshalling support
       a weee bit too copy pastey, but will do as version 0.1

--- internal/pkg/presentation/api/ngsi-ld/temporal.go
+++ internal/pkg/presentation/api/ngsi-ld/temporal.go
@@ -1,10 +1,12 @@
 package ngsild
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/url"
 
 	"github.com/diwise/context-broker/internal/pkg/application/cim"
+	"github.com/diwise/context-broker/pkg/ngsild/types"
 	"github.com/diwise/service-chassis/pkg/infrastructure/o11y"
 	"github.com/diwise/service-chassis/pkg/infrastructure/o11y/tracing"
 	"github.com/go-chi/chi/v5"
@@ -41,8 +43,8 @@ func NewRetrieveTemporalEvolutionOfAnEntityHandler(
 
 		traceID, ctx, log := o11y.AddTraceIDToLoggerAndStoreInContext(span, logger, ctx)
 
-		//var entityTemporal ngsitypes.EntityTemporal
-		_, err = contextInformationManager.RetrieveTemporalEvolutionOfEntity(ctx, tenant, entityID, propagatedHeaders)
+		var entityTemporal types.EntityTemporal
+		entityTemporal, err = contextInformationManager.RetrieveTemporalEvolutionOfEntity(ctx, tenant, entityID, propagatedHeaders)
 
 		if err != nil {
 			log.Error().Err(err).Msg("failed to retrieve temporal evolution of entity")
@@ -50,7 +52,16 @@ func NewRetrieveTemporalEvolutionOfAnEntityHandler(
 			return
 		}
 
+		responseBody, err := json.Marshal(entityTemporal)
+
+		if err != nil {
+			log.Error().Err(err).Msg("failed to convert or marshal response entity")
+			mapCIMToNGSILDError(w, err, traceID)
+			return
+		}
+
 		w.Header().Add("Content-Type", contentType)
 		w.WriteHeader(http.StatusOK)
+		w.Write(responseBody)
 	})
 }
